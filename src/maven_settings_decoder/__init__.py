123cbb9a1d284c14454644ebf23dcab1376a8649
Steven Van Ingelgem <steven.vaningelgem@bnpparibasfortis.com>
2024-12-02 07:44:55 +0100

    Cleanup code

--- src/maven_settings_decoder/__init__.py
+++ src/maven_settings_decoder/__init__.py
@@ -1,3 +1,3 @@
 from .decode import MavenDecodeError, MavenPasswordDecoder, MavenServer
 
-__all__ = ["MavenServer", "MavenPasswordDecoder", "MavenDecodeError"]
+__all__ = ["MavenDecodeError", "MavenPasswordDecoder", "MavenServer"]
